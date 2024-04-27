89925501091bfec424b2ebc4a1259c32e25c1524
Muharrem Bahçeci <muharrem2001bahceci@gmail.com>
2024-04-27 14:54:21 +0300

    envanter

---
+++ Assets/AudSys.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudSys : MonoBehaviour
+{
+    public Text volumeAmount;
+
+    public void SetAudio(float value)
+    {
+        AudioListener.volume = value;
+        volumeAmount.text = ((int)value * 100).ToString();
+    }
+}
