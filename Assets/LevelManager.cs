58fb4c39d7b85a74350441a387a86ecab2b2ace9
Umut Ali Coşkun <umutalicoskun@hotmail.com>
2024-04-27 21:27:49 +0300

    Girl Animations

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public Image timerBar;
     public ChibiManager chibi;
-    public Animator girl;
+    public SpriteRenderer girl;
+    public List<Sprite> girlSprites;
 
     public int day;
     public int progress;
@@ -16,6 +18,9 @@ public class LevelManager : MonoBehaviour
     public float timer;
     private float timerBarInitialWidth;
 
+    private float resetTimer;
+    private bool dead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +28,8 @@ public class LevelManager : MonoBehaviour
         timer = timerInitial;
 
         chibi.ShowChibi(day, progress);
+
+        girl.sprite = girlSprites[0];
     }
 
     // Update is called once per frame
@@ -31,12 +38,48 @@ public class LevelManager : MonoBehaviour
         if (timer > 0f)
         {
             timer -= Time.deltaTime;
+
+            if (0.25f > timer / timerInitial)
+            {
+                girl.sprite = girlSprites[3];
+            }
+            else if (0.5f > timer / timerInitial)
+            {
+                girl.sprite = girlSprites[2];
+            }
+            else if (0.75f > timer / timerInitial)
+            {
+                girl.sprite = girlSprites[1];
+            }
         }
         else
         {
-            Debug.Log("End");
+            LoseLevel();
         }
 
         timerBar.rectTransform.sizeDelta = new Vector2(timerBarInitialWidth * (timer / timerInitial), timerBar.rectTransform.sizeDelta.y);
+    }
+
+    public void LoseLevel()
+    {
+        girl.sprite = girlSprites[4];
+        Debug.Log("End");
+
+        if (dead)
+        {
+            if (resetTimer < 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                resetTimer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            dead = true;
+            resetTimer = 3f;
+        }
     }
 }
