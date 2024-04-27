484fe0e5560d72bd9004db9ddce649b68c7e065f
Umut Ali Coşkun <umutalicoskun@hotmail.com>
2024-04-27 04:20:57 +0300

    Timer

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public Image timerBar;
+
+    public float timerInitial;
+    
+    public float timer;
+    private float timerBarInitialWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerBarInitialWidth = timerBar.rectTransform.sizeDelta.x;
+        timer = timerInitial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("End");
+        }
+
+        timerBar.rectTransform.sizeDelta = new Vector2(timerBarInitialWidth * (timer / timerInitial), timerBar.rectTransform.sizeDelta.y);
+    }
+}
