6153c2d7fc7e6108c205a650601711f91ae68f6a
Umut Ali Coşkun <umutalicoskun@hotmail.com>
2024-04-28 15:03:01 +0300

    gun

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -8,8 +8,11 @@ public class LevelManager : MonoBehaviour
 {
     public Image timerBar;
     public ChibiManager chibi;
+
     public SpriteRenderer girl;
     public List<Sprite> girlSprites;
+
+    public GameObject chair;
 
     public int day;
     public int progress;
@@ -28,8 +31,6 @@ public class LevelManager : MonoBehaviour
         timer = timerInitial;
 
         chibi.ShowChibi(day, 0);
-
-        girl.sprite = girlSprites[0];
     }
 
     // Update is called once per frame
@@ -39,24 +40,50 @@ public class LevelManager : MonoBehaviour
         {
             timer -= Time.deltaTime;
 
-            if (0.2f > timer / timerInitial)
+            if (day == 0)
             {
-                girl.sprite = girlSprites[3];
-                chibi.ShowChibi(day, 1);
+                if (0.2f > timer / timerInitial)
+                {
+                    girl.sprite = girlSprites[3];
+                    chibi.ShowChibi(day, 2);
+                    chair.transform.eulerAngles = new Vector3(0f, 0f, 90f);
+                }
+                else if (0.4f > timer / timerInitial)
+                {
+                    girl.sprite = girlSprites[2];
+                }
+                else if (0.8f > timer / timerInitial)
+                {
+                    girl.sprite = girlSprites[1];
+                    chibi.ShowChibi(day, 1);
+                }
+                else
+                {
+                    girl.sprite = girlSprites[0];
+                }
             }
-            else if (0.4f > timer / timerInitial)
+            else if (day == 1)
             {
-                girl.sprite = girlSprites[2];
-            }
-            else if (0.8f > timer / timerInitial)
-            {
-                girl.sprite = girlSprites[1];
+                if (0.2f > timer / timerInitial)
+                {
+                    chibi.ShowChibi(day, 1);
+                }
+                else if (0.4f > timer / timerInitial)
+                {
+                    girl.sprite = girlSprites[6];
+                    chibi.ShowChibi(day, 1);
+                }
+                else
+                {
+                    girl.sprite = girlSprites[5];
+                    girl.transform.position = new Vector3(girl.transform.position.x, -0.55f, 0f);
+                }
             }
         }
         else
         {
             LoseLevel();
-            chibi.ShowChibi(day, 2);
+            chibi.ShowChibi(day, 3);
         }
 
         timerBar.rectTransform.sizeDelta = new Vector2(timerBarInitialWidth * (timer / timerInitial), timerBar.rectTransform.sizeDelta.y);
@@ -64,7 +91,15 @@ public class LevelManager : MonoBehaviour
 
     public void LoseLevel()
     {
-        girl.sprite = girlSprites[4];
+        if (day == 0)
+        {
+            girl.sprite = girlSprites[4];
+        }
+        else if (day == 1)
+        {
+
+        }
+        
         Debug.Log("End");
 
         if (dead)
